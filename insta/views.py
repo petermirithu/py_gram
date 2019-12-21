747f73732fb4f6f8e716ff7baa8a281e7336eb82
petermirithu <pyra_m.k@yahoo.com>
2019-12-21 13:06:34 +0300

    add view functions for home logout and profile page

--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import logout
+def home(request):
+  '''
+  view function that renders the home page
+  '''
+  return render(request,'index.html')
 
-# Create your views here.
+def logout_request(request):
+  '''
+  view function renders home page once logout
+  '''
+  logout(request)
+  return redirect('Home')
+
+def profile(request):
+  '''
+  view function that renders profile page
+  '''
+  title="Profile"
+  return render(request,'profile.html',{"title":title})
+
+
+
