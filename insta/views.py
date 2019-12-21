c914f6e23abdcdfc49e7d4459f7d2e34191d4614
petermirithu <pyra_m.k@yahoo.com>
2019-12-21 18:51:26 +0300

    create view functions for update profile page

--- insta/views.py
+++ insta/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import logout
+from django.contrib.auth.models import User
+from .models import UserProfile
+from .forms import UpdateProfileForm,UserUpdateForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 def home(request):
   '''
   view function that renders the home page
@@ -13,12 +19,38 @@ def logout_request(request):
   logout(request)
   return redirect('Home')
 
+@login_required(login_url="/accounts/login/")
 def profile(request):
   '''
   view function that renders profile page
   '''
-  title="Profile"
+  title="Profile"      
   return render(request,'profile.html',{"title":title})
+
+@login_required(login_url="/accounts/login/")
+def update_profile(request):
+  '''
+  view function that renders the updat profile form
+  '''      
+  if request.method=='POST':
+    user_form=UserUpdateForm(request.POST, instance=request.user)
+    profile_form=UpdateProfileForm(request.POST, request.FILES, instance=request.user.userprofile)
+
+    if user_form.is_valid() and profile_form.is_valid():
+      user_form.save()
+      profile_form.save()            
+      return redirect("profile")
+  else:
+    user_form=UserUpdateForm(instance=request.user)
+    profile_form=UpdateProfileForm(instance=request.user.userprofile)
+
+  context={
+    'user_form':user_form,
+    'profile_form':profile_form,
+  }
+
+  return render(request,'updateprofile.html',context)    
+
 
 
 
