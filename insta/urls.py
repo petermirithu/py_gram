dbfdbad70a59d5e7d2485f2392d67fce84c46701
petermirithu <pyra_m.k@yahoo.com>
2019-12-21 13:06:16 +0300

    add url patterns for home logout and profile page

---
+++ insta/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url('^$',views.home,name="Home"),
+    url(r'^logout/$',views.logout_request,name="logout"),
+    url(r'accounts/profile/$',views.profile,name="profile"),
+
+]
