d67298d4766af05f248a7b7bddf8c8300adc8141
petermirithu <pyra_m.k@yahoo.com>
2019-12-21 18:49:25 +0300

    register signals for the app to use

--- insta/apps.py
+++ insta/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig
 
 class InstaConfig(AppConfig):
     name = 'insta'
+
+# importing signals for the app to use in creating profile page automatically
+    def ready(self):
+        import insta.signals
+
