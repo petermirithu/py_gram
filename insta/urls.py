fc64a0b9b9a4f39b1dc429a9db26680a15dac699
petermirithu <pyra_m.k@yahoo.com>
2019-12-21 18:50:57 +0300

    add an update_profile url

--- insta/urls.py
+++ insta/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url('^$',views.home,name="Home"),
     url(r'^logout/$',views.logout_request,name="logout"),
     url(r'accounts/profile/$',views.profile,name="profile"),
-
+    url(r'^update_profile/$',views.update_profile,name="updateProfile"),    
 ]
