20430b309b7a0a5b2baebe86bc962cce72274804
petermirithu <pyra_m.k@yahoo.com>
2019-12-23 09:25:53 +0300

    add view functions for creating anew post and viewing a single post

--- insta/views.py
+++ insta/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import logout
 from django.contrib.auth.models import User
-from .models import UserProfile
-from .forms import UpdateProfileForm,UserUpdateForm
+from .models import UserProfile,ImagePost,Comments
+from .forms import UpdateProfileForm,UserUpdateForm,ImagePostForm,ImageCommentForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-
+from django.http import Http404,HttpResponseRedirect,HttpResponse
 def home(request):
   '''
   view function that renders the home page
   '''
-  return render(request,'index.html')
+  posts=ImagePost.get_images_all()
+  return render(request,'index.html',{"posts":posts})
 
 def logout_request(request):
   '''
@@ -30,7 +31,7 @@ def profile(request):
 @login_required(login_url="/accounts/login/")
 def update_profile(request):
   '''
-  view function that renders the updat profile form
+  view function that renders the update profile form
   '''      
   if request.method=='POST':
     user_form=UserUpdateForm(request.POST, instance=request.user)
@@ -51,6 +52,46 @@ def update_profile(request):
 
   return render(request,'updateprofile.html',context)    
 
+@login_required(login_url="/accounts/login/")
+def new_post(request):
+  '''
+  view function that renders a form for creating a new post
+  '''  
+  if request.method=='POST':
+    form=ImagePostForm(request.POST,request.FILES)
+    if form.is_valid():
+      post=form.save(commit=False)
+      post.posted_by=request.user
+      post.save()
+
+      return redirect('Home')
+  else:
+    form=ImagePostForm()    
+
+  return render(request,'new_post.html',{"form":form})  
+
+@login_required(login_url="/accounts/login/")
+def single_post(request, id):
+  '''
+  view function that renders a single post and  comments sections
+  '''  
+  if request.method=='POST':
+    form=ImageCommentForm(request.POST)
+    if form.is_valid():
+      comment=form.save(commit=False)
+      comment.posted_by=request.user      
+      post=ImagePost.objects.get(id=id)
+      comment.image_id=post
+      comment.save()
+      HttpResponseRedirect('single_post')
+  else:
+    form=ImageCommentForm()
+
+  image_posted=ImagePost.single_image(id)  
+  imageId=ImagePost.get_image_id(id)
+  comments=Comments.get_user_comments(imageId)
+      
+  return render(request,'single_post.html',{"form":form,"comments":comments,"post":image_posted})      
 
 
 
