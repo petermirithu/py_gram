8c228b67d7f5f732fd574af18155a0655d050c2a
petermirithu <pyra_m.k@yahoo.com>
2019-12-23 09:25:12 +0300

    add url for new apost and path for single post viewing

--- insta/urls.py
+++ insta/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
 from . import views
+from django.urls import path
 
 urlpatterns = [
     url('^$',views.home,name="Home"),
     url(r'^logout/$',views.logout_request,name="logout"),
     url(r'accounts/profile/$',views.profile,name="profile"),
-    url(r'^update_profile/$',views.update_profile,name="updateProfile"),    
+    url(r'^update_profile/$',views.update_profile,name="updateProfile"),  
+    url(r'^new_post/$',views.new_post,name="newPost"),
+    path('single_post/<int:id>',views.single_post,name="singlePost"),
 ]
