ce178d9939532d2c3d4a463db9e3fbe9670ef9c0
petermirithu <pyra_m.k@yahoo.com>
2019-12-23 09:24:09 +0300

    add imagepost form and comments form

--- insta/forms.py
+++ insta/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import UserProfile
+from .models import UserProfile,ImagePost,Comments
 from django.contrib.auth.models import User
 
 class UpdateProfileForm(forms.ModelForm):
@@ -17,7 +17,23 @@ class UserUpdateForm(forms.ModelForm):
   class Meta:
     model=User
     fields=['username','email']
-    
+
+class ImagePostForm(forms.ModelForm):
+  '''
+  class that defines how the post form shall look like
+  '''
+  class Meta:
+    model=ImagePost
+    exclude=['posted_on','posted_by']
+class ImageCommentForm(forms.ModelForm):
+  '''
+  class that defines how the comment form shall look like
+  '''
+  class Meta:
+    model=Comments
+    exclude=['posted_on','image_id','posted_by']
+
+
 
 
   
