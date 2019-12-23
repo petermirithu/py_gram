7392e381f570fb73a8abdf15b7098b52569d8816
petermirithu <pyra_m.k@yahoo.com>
2019-12-23 09:26:11 +0300

    add tinymce

--- py_gram/urls.py
+++ py_gram/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     url(r'admin/', admin.site.urls),
     url(r'', include('insta.urls')),
     url(r'^accounts/',include('registration.backends.simple.urls')),
+    url(r'tinymce', include('tinymce.urls')),     
 ]
