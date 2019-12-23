eb81a2420107964353b1929bfd59fbc2d83dbb0b
petermirithu <pyra_m.k@yahoo.com>
2019-12-23 23:26:41 +0300

    register imagepost and comment model

--- insta/admin.py
+++ insta/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile,ImagePost,Comments
 
 admin.site.register(UserProfile)
+admin.site.register(ImagePost)
+admin.site.register(Comments)
 
 
