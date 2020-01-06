480340446b04ad8b00162cc59e46c4c4adebc6cd
Pyra <55141332+petermirithu@users.noreply.github.com>
2020-01-06 17:56:03 +0300

    Update tests.py

--- insta/tests.py
+++ insta/tests.py
@@ -1,49 +1,7 @@
 from django.test import TestCase
 from .models import UserProfile,ImagePost,Comments
 from django.contrib.auth.models import User
-
-class UserProfileTestCase(TestCase):
-  '''
-  class that tests all function in the userprofile
-  '''
-  def setUp(self):
-    '''
-    Testcase to create new user profile object for test purposes
-    '''    
-    self.pyra=User(username='pyra',email='pyra@yahoo.com')
-    self.pyra=UserProfile(user=self.pyra,status='Feel goood',pic="pyra.png",career='Software dev')
-
-  def test_save_profile(self):
-    '''
-    testcase that tests on saving a user's profile
-    '''
-    self.pyra.save_profile()
-    users=UserProfile.objects.all()
-    self.assertEquals(len(users,1))
-
-  def tearDown(self):   
-    '''
-    Testcase that delete all objects after every test has run
-    ''' 
-    User.objects.all().delete()  
-    UserProfile.all().delete()
-
-  def test_delete_profile(self):
-    '''
-    test case that tests on deleting a user's profile
-    '''
-    self.pyra.save_profile()
-    users=UserProfile.objects.all()
-    self.assertTrue(len(users)==0)
-
-  def test_search_user(self):
-    '''
-    test case that tests on searching for a single user
-    '''
-    self.pyra.save_profile()
-    found=UserProfile.search_user(self.pyra.username)
-    self.assertTrue(found.username=='pyra')
-
+  
 class ImagePostTestCase(TestCase):
   '''
   class with testcases for all function in imagepost class
@@ -55,7 +13,7 @@ class ImagePostTestCase(TestCase):
     self.pyra=User(username='pyra',email='pyra@yahoo.com')
     self.pyra.save()
 
-    self.bmw=ImagePost(image=' https://ucarecdn.com/505a47e6-769a-40d7-a4ac-b0dea6822723/',image_name="BMW series",caption='I love BMW cars',posted_by=self.pyra,posted_on='12-10-2019')
+    self.bmw=ImagePost(image='https://ucarecdn.com/505a47e6-769a-40d7-a4ac-b0dea6822723/',image_name="BMW series",caption='I love BMW cars',posted_by=self.pyra,posted_on='12-10-2019')
 
   def test_save_post(self):
     '''
@@ -104,15 +62,7 @@ class ImagePostTestCase(TestCase):
     self.bmw.save_post()
     got=ImagePost.single_image(self.bmw.id)  
     self.assertTrue(got.image_name==self.bmw.image_name)
-
-  def test_get_image_id(self):
-    '''
-    testcase that tests on getting an image id
-    '''
-    self.bmw.save_post()
-    post_id=ImagePost.get_image_id(self.bmw.id)  
-    self.assertTrue(post_id.id==self.bmw.id)
-
+  
   def test_get_user_posts(self):
     '''
     testcase that tests on getting a user's posts
@@ -130,6 +80,27 @@ class ImagePostTestCase(TestCase):
     found=ImagePost.get_posts_by_name('BMW series')
     self.assertEquals(len(found),1)
 
+class UserProfileTestCase(TestCase):
+  '''
+  class that tests all function in the userprofile
+  '''
+  def setUp(self):
+    '''
+    Testcase to create new user profile object for test purposes
+    '''    
+    self.u_pyra=User(username='pyra',email='pyra@yahoo.com')
+    self.u_pyra.save()
+
+    self.pyra=UserProfile(user=self.u_pyra,status='Feel goood',pic="https://ucarecdn.com/505a47e6-769a-40d7-a4ac-b0dea6822723/",career='Software dev')
+    self.pyra.save()
+
+  def tearDown(self):   
+    '''
+    Testcase that delete all objects after every test has run
+    ''' 
+    User.objects.all().delete()  
+    UserProfile.all().delete()
+    
 class CommentsTestCase(TestCase):
   '''
   class that contains testcases for all function under comments class
@@ -138,11 +109,14 @@ class CommentsTestCase(TestCase):
     '''
     creates comment object for test purposes
     '''
-    self.pyra=User(username='pyra',email='pyra@yahoo.com')
-    self.bmw=ImagePost(image=' https://ucarecdn.com/505a47e6-769a-40d7-a4ac-b0dea6822723/',image_name="BMW series",caption='I love BMW cars',posted_by=self.pyra,posted_on='12-10-2019')
-    self.bmw.save_post()
-    self.pyra.save()
-    self.comment=Comments(body="I like it",image_id=self.bmw.id,posted_by=self.pyra,posted_on='09-12-2019')    
+    self.u_pyra=User(username='pyra',email='pyra@yahoo.com')
+    self.u_pyra.save()
+
+    self.bmw=ImagePost(image=' https://ucarecdn.com/505a47e6-769a-40d7-a4ac-b0dea6822723/',image_name="BMW series",caption='I love BMW cars',posted_by=self.u_pyra,posted_on='12-10-2019')
+    self.bmw.save()
+    
+    self.comment=Comments(body="I like it",image_id=self.bmw,posted_by=self.u_pyra,posted_on='09-12-2019')  
+    self.comment.save()  
 
   def test_save_comment(self):
     '''
@@ -150,7 +124,7 @@ class CommentsTestCase(TestCase):
     '''    
     self.comment.save_comment()
     comments=Comments.objects.all()
-    self.assertEquals(len(comments)==1)
+    self.assertEquals(len(comments),1)
 
   def tearDown(self):   
     '''
@@ -158,26 +132,18 @@ class CommentsTestCase(TestCase):
     '''     
     ImagePost.objects.all().delete()
     User.objects.all().delete()
-    Comment.objects.all().delete()
+    Comments.objects.all().delete()
         
   def test_delete_comment(self):
     '''
     testcase to test on deleting a comment
     '''
-    self.pyra.save()
+    self.u_pyra.save()
+    self.bmw.save()
     self.comment.save_comment()    
     self.comment.delete_comment()
     comments=Comments.objects.all()
-    self.assertTrue(len(comments)==0)
-
-  def test_get_posts_comments(self):
-    '''
-    testcase to test on getting comments for a specific post
-    '''
-    self.bmw.save_post()
-    self.comment.save_comment()
-    comments_found=Comments.get_post_comments(self.bmw.id)
-    self.assertEquals(len(comments_found),1)
+    self.assertTrue(len(comments)==0) 
 
   def test_update_caption(self):
     '''
